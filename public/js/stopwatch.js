f4e7a33aa8a3fefbaff5c1cb3cc477d6ac268426
Jack2ee-dev <jack2ee.dev@gmail.com>
2021-07-13 14:38:55 +0900

    feat: Implement timeFlies function
    100ms마다 시간 체크 함수 개발

--- public/js/stopwatch.js
+++ public/js/stopwatch.js
@@ -11,6 +11,28 @@ const timer = (() => {
   const STATUS_STOP = 'STOP';
 
   let currentStatus = STATUS_INIT;
+  let timerId;
 
-	
+  const timeFlies = () => {
+    const ct = { ...currentTime };
+
+    if (ct.millisecond === 90) {
+      ct.second += 1;
+      ct.millisecond = 0;
+    }
+
+    if (ct.second === 59) {
+      ct.minute += 1;
+      ct.second = 0;
+    }
+
+    if (ct.minute === 59) {
+      ct.hour += 1;
+      ct.minute = 0;
+    }
+
+    ct.millisecond += 10;
+
+    currentTime = ct;
+  };
 })();
