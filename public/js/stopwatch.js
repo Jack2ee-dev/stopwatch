0f3dc8b12e4651f25f34ba80903f1259abd16110
passersby-1 <kor3166@gmail.com>
2021-07-13 16:13:30 +0900

    feat: refactoring code
    1. index.html 버튼, 기본 disabled 속성 제거
    2. stopwatch, timer ms 2자리로 출력되는거 수정

--- public/js/stopwatch.js
+++ public/js/stopwatch.js
@@ -5,80 +5,6 @@ let currentTime = {
   millisecond: 0,
 };
 
-const timer = (() => {
-  const $stopwatchTime = document.querySelector('.stopwatch-time');
-  const $lapBtn = document.querySelector('.lap-btn');
-  const $startResumeStopBtn = document.querySelector('.start-resume-stop-btn');
-  const $resetBtn = document.querySelector('.reset-btn');
-
-  let timerId;
-
-  (() => {
-    $lapBtn.setAttribute('disabled', '');
-    $resetBtn.setAttribute('disabled', '');
-  })();
-
-  const timerAction = () => {
-    const timeFlies = () => {
-      const updateTime = () => {
-        const format = n => (n < 10 ? '0' + n : n + '');
-        const { hour, minute, second, millisecond } = currentTime;
-
-        $stopwatchTime.textContent = `${format(hour)}:${format(
-          minute
-        )}:${format(second)}.${millisecond}`;
-      };
-
-      const ct = { ...currentTime };
-      if (ct.millisecond === 90) {
-        ct.second += 1;
-        ct.millisecond = 0;
-      }
-
-      if (ct.second === 59) {
-        ct.minute += 1;
-        ct.second = 0;
-      }
-
-      if (ct.minute === 59) {
-        ct.hour += 1;
-        ct.minute = 0;
-      }
-
-      ct.millisecond += 10;
-
-      currentTime = ct;
-      updateTime();
-    };
-
-    const start = () => {
-      timerId = setInterval(timeFlies, 100);
-      $startResumeStopBtn.textContent = 'STOP';
-      $lapBtn.removeAttribute('disabled');
-      $resetBtn.removeAttribute('disabled');
-    };
-
-    const stop = () => {
-      clearInterval(timerId);
-      timerId = null;
-      $startResumeStopBtn.textContent = 'RESUME';
-      $lapBtn.removeAttribute('disabled');
-      $resetBtn.removeAttribute('disabled');
-    };
-
-    const resume = () => {
-      timerId = setInterval(timeFlies, 100);
-      $startResumeStopBtn.textContent = 'STOP';
-    };
-
-    if ($startResumeStopBtn.textContent === 'START') start();
-    else if ($startResumeStopBtn.textContent === 'STOP') stop();
-    else if ($startResumeStopBtn.textContent === 'RESUME') resume();
-  };
-
-  $startResumeStopBtn.onclick = timerAction;
-})();
-
 const lap = (() => {
   const $lapBtn = document.querySelector('.lap-btn');
   const $stopwatchTime = document.querySelector('.stopwatch-time');
@@ -148,3 +74,75 @@ const reset = (() => {
     toggleDisabled,
   };
 })();
+
+const timer = (() => {
+  const $stopwatchTime = document.querySelector('.stopwatch-time');
+  const $lapBtn = document.querySelector('.lap-btn');
+  const $startResumeStopBtn = document.querySelector('.start-resume-stop-btn');
+  const $resetBtn = document.querySelector('.reset-btn');
+
+  let timerId;
+
+  const timerAction = () => {
+    const timeFlies = () => {
+      const updateTime = () => {
+        const format = n => (n < 10 ? '0' + n : n + '');
+        const { hour, minute, second, millisecond } = currentTime;
+
+        $stopwatchTime.textContent = `${format(hour)}:${format(
+          minute
+        )}:${format(second)}.${millisecond}`;
+      };
+
+      const ct = { ...currentTime };
+      if (ct.millisecond === 9) {
+        ct.second += 1;
+        ct.millisecond = 0;
+      }
+
+      if (ct.second === 59) {
+        ct.minute += 1;
+        ct.second = 0;
+      }
+
+      if (ct.minute === 59) {
+        ct.hour += 1;
+        ct.minute = 0;
+      }
+
+      ct.millisecond += 1;
+
+      currentTime = ct;
+      updateTime();
+    };
+
+    const start = () => {
+      timerId = setInterval(timeFlies, 100);
+      $startResumeStopBtn.textContent = 'STOP';
+      $lapBtn.removeAttribute('disabled');
+      $resetBtn.removeAttribute('disabled');
+    };
+
+    const stop = () => {
+      clearInterval(timerId);
+      timerId = null;
+      $startResumeStopBtn.textContent = 'RESUME';
+      $lapBtn.removeAttribute('disabled');
+      $resetBtn.removeAttribute('disabled');
+    };
+
+    const resume = () => {
+      timerId = setInterval(timeFlies, 100);
+      $startResumeStopBtn.textContent = 'STOP';
+    };
+
+    if ($startResumeStopBtn.textContent === 'START') start();
+    else if ($startResumeStopBtn.textContent === 'STOP') stop();
+    else if ($startResumeStopBtn.textContent === 'RESUME') resume();
+  };
+
+  $startResumeStopBtn.onclick = timerAction;
+})();
+
+lap.toggleDisabled();
+reset.toggleDisabled();
