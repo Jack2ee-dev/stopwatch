a9a306247af9c73927ce40ccf1e73e9883736e94
Jack2ee-dev <jack2ee.dev@gmail.com>
2021-07-13 14:25:06 +0900

    feat: Set initial states for timer start
    전역 상태인 currentTime과 start 함수의 상태의 currentStatus 설정

---
+++ public/js/stopwatch.js
@@ -0,0 +1,14 @@
+let currentTime = {
+  hour: 0,
+  minute: 0,
+  second: 0,
+  millisecond: 0,
+};
+
+const start = (() => {
+  const STATUS_INIT = 'INIT';
+  const STATUS_START = 'START';
+  const STATUS_STOP = 'STOP';
+
+  let currentStatus = STATUS_INIT;
+})();
