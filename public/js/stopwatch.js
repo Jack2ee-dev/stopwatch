f7ff7b2968f8e21ef56970e28edf08b352e36231
Jack2ee-dev <jack2ee.dev@gmail.com>
2021-07-13 14:50:27 +0900

    feat: Implement timer start function
    100ms 진행되는 timer start 함수 작성 완료

--- public/js/stopwatch.js
+++ public/js/stopwatch.js
@@ -10,29 +10,42 @@ const timer = (() => {
   const STATUS_START = 'START';
   const STATUS_STOP = 'STOP';
 
+  const $lapBtn = document.querySelector('.lap-btn');
+  const $startResumeStopBtn = document.querySelector('.start-resume-stop-btn');
+  const $resetBtn = document.querySelector('.reset-btn');
+
   let currentStatus = STATUS_INIT;
   let timerId;
 
-  const timeFlies = () => {
-    const ct = { ...currentTime };
+  (() => {
+    $lapBtn.setAttribute('disabled', '');
+    $resetBtn.setAttribute('disabled', '');
+  })();
 
-    if (ct.millisecond === 90) {
-      ct.second += 1;
-      ct.millisecond = 0;
-    }
+  const start = () => {
+    const timeFlies = () => {
+      const ct = { ...currentTime };
+      if (ct.millisecond === 90) {
+        ct.second += 1;
+        ct.millisecond = 0;
+      }
 
-    if (ct.second === 59) {
-      ct.minute += 1;
-      ct.second = 0;
-    }
+      if (ct.second === 59) {
+        ct.minute += 1;
+        ct.second = 0;
+      }
 
-    if (ct.minute === 59) {
-      ct.hour += 1;
-      ct.minute = 0;
-    }
+      if (ct.minute === 59) {
+        ct.hour += 1;
+        ct.minute = 0;
+      }
 
-    ct.millisecond += 10;
+      ct.millisecond += 10;
 
-    currentTime = ct;
+      currentTime = ct;
+    };
+    timerId = setInterval(timeFlies, 100);
   };
+
+  $startResumeStopBtn.onclick = start;
 })();
